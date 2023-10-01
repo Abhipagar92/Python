89684578b7dddf1316068189c445b747884c6ff4
Abhishek Pagar <78256435+Abhipagar92@users.noreply.github.com>
2023-10-01 06:10:39 +0000

    variable studied

---
+++ 1First.py
@@ -0,0 +1,10 @@
+# printing statements
+
+print("hello python")
+print(10)
+print('Abhi','Lakhan','Mahesh','Om','Praju','Gau','Pooja','Gargu')
+
+
+#comments in Python
+
+# this is comment
---
+++ 2.variables.py
@@ -0,0 +1,64 @@
+
+# variables: Variables are containers for storing data values.
+
+#     * python has no cammand for declareing vatiable.
+#     * a variable is created the moment you first assign a value t it. 
+#     * variable do not need to be declare with any perticular type and even change after they have been set
+
+#example
+
+x = 4                # x is of type int
+print(x)    
+
+x = "Abhi"           # x is now type str
+print(x)
+
+#-----------------------------------------------------------------------------------------------------------------------
+#Casting : if you want to specify the data type of a variable,this can be done with Casting
+#example
+
+x = str("cast") 
+y = int(3) 
+z = float(3.1)  
+
+print(x)
+print(y)
+print(z)
+
+#---------------------------------------------------------------------------------------------------------------------------
+#Get the type: You can get the type of a variable with the  type() function
+#example
+
+x = 5
+y = "Abhi"
+print(type(x))
+print(type(y))
+
+
+#string variable can be declare either single('') or double quote(" ")
+
+#varible names are case sensitive
+#Example
+a=10
+A="Abhi"
+print(a)
+print(A)
+
+#----------------------------------------------------------------------------------------------------------
+# variable name examples
+#variable name can only contain alphanumeric characters and underscores( A-a, 0-9 , _ )
+
+myvar   ="abhi"
+my_var  = "abhi"
+_my_var = "abhi"
+myVar   = "abhi"
+MyVar   = "abhi"
+myvar2  = "abhi"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myvar)
+print(MyVar)
+print(myvar2)
