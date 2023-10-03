b272c52753f718397c23e3412dee0c39d1d2441a
Abhishek Pagar <78256435+Abhipagar92@users.noreply.github.com>
2023-10-03 10:32:07 +0000

    changes made

--- 3.data _types.py
+++ 3.data _types.py
@@ -9,8 +9,79 @@
 # Binary Types:	bytes, bytearray, memoryview
 # None Type:	NoneType
 
-a = 10
+#- OBJECT ----------------------------------- DATA TYPE ------------------ OUTPUT-------------------------------------------
+ 
+a = 5                                           #int     
+print(type(a))                  
+
+a = "hello abhi"                                #str
+print(type(a))
+
+a = 5.5                                         #float
+print(type(a))
+
+a = 1j                                          #complex # for complex number always use 'j' in the object insteat of 'i or anything'
+print(type(a))
+
+a = ['abhi','lakhan','ram','seeta','hanuman']   #list 
+print(type(a))
+
+a = ('abhi','lakhan','ram','seeta','hanuman')   #tuple
+print(type(a))
+
+a = range(5)                                    #range
+print(type(a))
+
+a = {'name':'lakhan','age':22 }                 #dict
+print(type(a))
+
+a = {'abhi','lakhan','ram','seeta','hanuman'}   #set
+print(type(a))
+
+a = frozenset ({'abhi','lakhan','ram','seeta','hanuman'}) #frozenset
+print(type(a))
+
+a = True                                        #bool
+print(type(a))
+
+a = b"hello"                                    #bytes
+print(type(a))
+
+a = bytearray(5)                                #bytearray
+print(type(a))
+
+a = memoryview(bytes(5))                        #menoryview
+print(type(a))
+
+a = None                                        #none 
+print(type(a))
+
+
+a = 5
 print(int(a))
+
+a = "Hello abhi"
+print(str(a))
+
+a = 5.5
+print(float(a))
+
+a = 5j
+print(complex(a))
+
+a = ['ram', 'seeta' , 'hanuman']
+print(list(a))
+
+a = ('ram', 'seeta' , 'hanuman')
+print(tuple(a))
+
+a = range(5)
+print
+
+
+
+
+
 
 
 
